e2302e54787d905ff18ac975ca84b247c83bd111
glebb1331 <wertyska112@mail.ru>
2025-11-02 16:42:23 +0300

    increment 3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,20 +5,15 @@ import (
 	"net/http"
 
 	"github.com/glebb1331/shortener-practicum/internal/handler"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost && r.URL.Path == "/" {
-			handler.ShortenHandler(w, r)
-			return
-		}
-		if r.Method == http.MethodGet && r.URL.Path != "/" {
-			handler.RedirectHandler(w, r)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-	})
+	r := chi.NewRouter()
+
+	r.Post("/", handler.ShortenHandler)
+
+	r.Get("/{id}", handler.RedirectHandler)
 
 	log.Println("Сервер запущен на http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
