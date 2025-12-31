55eaae01ba3e33038fe351e7f9f5e32dfc9c2f65
glebb1331 <wertyska112@mail.ru>
2025-12-31 15:15:40 +0300

    test commit 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -41,14 +41,11 @@ func main() {
 	r.Post("/api/shorten", h.APIShortenHandler)
 	r.Post("/api/shorten/batch", h.APIShortenBatchHandler)
 
-	// Эндпоинт для истории URL пользователя
 	r.Get("/api/user/urls", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		// Если данных нет, по спецификации часто требуется 204 No Content
 		w.WriteHeader(http.StatusNoContent)
 	})
 
-	// В самом конце — жадный поиск по ID
 	r.Post("/", h.ShortenHandler)
 	r.Get("/{id}", h.RedirectHandler)
 
